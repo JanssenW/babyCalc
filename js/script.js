ecccfd6c6ce35867fe08b4a6036f74a2dc37f09e
Janssen Wilgner <wilgnerjo@gmail.com>
2021-12-22 08:07:30 -0300

    BabyCalc First Commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var btnMM = document.querySelector('#btnMM');
+var btnMS = document.querySelector('#btnMS');
+var content = document.querySelector('#content');
+var p = document.querySelector('p');
+
+btnMM.addEventListener('click', ()=>{
+    p.innerText = 'Hello World';
+
+});
+
---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  var canvas = createCanvas(400, 400);
+  canvas.parent('canvas');
+  canvas.
+}
+
+function draw() {
+  background(127)
+  rect(100,100, 100, 60)
+}
+
+
+f
