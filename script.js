77c1979362aedd53e59c5e1cb151fae2c299c03e
Iago Regiani <iagoregiani@gmail.com>
2021-12-24 17:52:08 -0300

    Initial refactor
    Removed external libs
    Added placeholder items elements
    Added placeholder functions

--- script.js
+++ script.js
@@ -1,110 +1,19 @@
-var canvas = document.getElementById('canvas');
+const heroContainer = document.getElementById("heroContainer")
+const heros = {};
 
-/*
-var grid = document.querySelector('.container');
-var grid = document.getElementsByClassName('.container');
-var addB = document.getElementById("addB")
+function saveHero() {
 
-
-/*var addB = document.querySelector('#addB');
-addB.addEventListener('click', ()=>{
-grid.
-
-});
-
-
-addB.addEventListener('click', ()=>{
-    
-    
-    });
-
-
-let btnMM = document.querySelector('#btnMM');
-let btnMS = document.querySelector('#btnMS');
-let content = document.querySelector('#content');
-let p = document.querySelector('p');
-
-btnMM.addEventListener('click', ()=>{
-    p.innerText = 'Hello World';
-
-});
-
-
-var canvas;
-var stage;
-var square;
-
-function createRect(x, y){
-
-    this.x = x;
-    this.y = y;
-    var w = 100;
-    var h = 200;
 }
 
+function deleteHero() {
 
-function createChar(price, level, costLevel){
-
-    this.price = price;
-    this.level = level;
-    this.costLevel = costLevel;
-    
 }
 
+function createHero() {
+    console.log('Creating hero!')
+    const newHero = ``
+        // heroContainer.appendChild()
 
-function draw() {
-    var canvas = document.getElementById('canvas');
-    if (canvas.getContext) {
-      var context = canvas.getContext('2d');
-  
-      context.fillRect(20,20,100,100);
-      context.clearRect(40,40,60,60);
-      context.strokeRect(45,45,50,50);
-    }
-  }
-
-
-function init() {   
-    canvas = document.getElementById("canvas");
-    stage = new createjs.Stage(canvas);
 }
 
-
-
-function createSquare(){
-    square = new createjs.Shape();
-    square.graphics.beginFill("red").drawRect(50, 0, 50, 50)
-    square.x = Math.random() * canvas.width;
-        square.y = Math.random() * canvas.height;
-    stage.addChild(square);
-    stage.update();
-}
-
-window.onload = init();
-
-createSquare();
-
-
-
-
-
-
-
-      <ul>
-               <div class="element">
-                <li><canvas id="canvas"></canvas></li>
-                <li><canvas id="canvas"></canvas></li>
-    
-                </canvas></li>
-
-                <div class="element">
-                    <li><canvas id="canvas">
-    
-                    </canvas></li>
-            </div>
-            
-        </ul>
-
-*/
-
-
+createHero()
